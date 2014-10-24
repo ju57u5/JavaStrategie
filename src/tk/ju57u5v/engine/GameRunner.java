f9c39ce8184f4d51365f34fd8ebf15b2902fa096
Justus <Justus@Justus-PC.speedport.ip>
2014-10-24 20:13:50 +0200

    Resourcen können geladen werden.

--- src/tk/ju57u5v/engine/GameRunner.java
+++ src/tk/ju57u5v/engine/GameRunner.java
@@ -1,5 +1,14 @@
 package tk.ju57u5v.engine;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 public class GameRunner extends Thread {
 	
+	
+	public void run() {
+		
+		
+		
+	}
 }
--- src/tk/ju57u5v/engine/ResourceManager.java
+++ src/tk/ju57u5v/engine/ResourceManager.java
@@ -1,5 +1,28 @@
 package tk.ju57u5v.engine;
 
-public class ResourceManager {
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.imageio.ImageIO;
+
+public class ResourceManager {
+	
+	Map<String,BufferedImage> textures = new HashMap<String,BufferedImage>();
+	File appdata = new File((System.getenv("APPDATA")));
+	File gamePath = new File(appdata,"/JavaStrategie");
+	File texturePath = new File(gamePath, "/textures");
+	
+	public BufferedImage getResource(String pQuery) {
+		return textures.get(pQuery);
+	}
+	
+	public void loadImage(String pResourceName, String pQuery) {
+		File imagePath = new File(texturePath,"/"+pResourceName);
+		try {
+			textures.put(pQuery, ImageIO.read(imagePath));
+		} catch(IOException exeption) {}
+	}
 }
