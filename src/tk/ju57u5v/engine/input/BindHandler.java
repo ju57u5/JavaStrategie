352913ecfa3012a7a3c7db9410abf95779e9792d
Justus <Justus@Justus-PC.speedport.ip>
2014-12-21 22:38:41 +0100

    KeyHandler umbenannt

--- src/tk/ju57u5v/engine/input/BindHandler.java
+++ src/tk/ju57u5v/engine/input/BindHandler.java
@@ -1,5 +1,5 @@
 package tk.ju57u5v.engine.input;
 
-public class KeyHandler {
+public class BindHandler {
 
 }
--- src/tk/ju57u5v/engine/input/BindListener.java
+++ src/tk/ju57u5v/engine/input/BindListener.java
@@ -1,5 +1,5 @@
 package tk.ju57u5v.engine.input;
 
-public interface KeyListener {
+public interface BindListener {
 
 }
